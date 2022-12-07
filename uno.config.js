cd034b7f38f6a83004e44d48fbf37d73f75a708c
treetzeng <treetzeng@lctech.com.tw>
2022-12-07 17:55:16 +0800

    Revert "feat: update config"
    This reverts commit 5ce2006fc888d0556943d5beaa6e84af26bd8c98.

--- uno.config.js
+++ uno.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'unocss';
 import presetUno from '@unocss/preset-uno';
 import presetAttributify from '@unocss/preset-attributify';
 import transformerDirectives from '@unocss/transformer-directives';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
 
 export default defineConfig({
   presets: [presetUno(), presetAttributify()],
-  transformers: [transformerVariantGroup(), transformerDirectives()],
+  transformers: [transformerDirectives()],
 });
